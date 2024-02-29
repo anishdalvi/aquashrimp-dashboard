878003c37bab3aaf4d7949c9684f6b8045064ca1
student <anish.201914101@vcet.edu.in>
2024-02-29 13:50:37 +0530

    Temperature Formatted in 1:47:59 PM

--- backend/controllers/aquaController.js
+++ backend/controllers/aquaController.js
@@ -1,5 +1,5 @@
 const asyncHandler = require("express-async-handler");
-const Aqua = require("../models/aquaModel");
+const Data = require("../models/aquaModel");
 const axios = require('axios');
 
 const BLYNK_TOKEN = process.env.BLYNK_TOKEN;
@@ -12,12 +12,17 @@ const fetchData = async () => {
     const response2 = await axios.get(`https://blynk.cloud/external/api/get?token=${BLYNK_TOKEN}&v6`);
     const data2 = response2.data;
 
-    const newData = new Aqua({
+    // Format time as Hour:Min:Seconds with AM/PM
+    const currentTime = new Date();
+    const formattedTime = currentTime.toLocaleTimeString('en-US', { hour12: true });
+
+    const newData = new Data({
       Temperature: data1,
       PH: data2,
-      time: Date.now(),
+      time: formattedTime,
     });
     await newData.save();
+    // console.log(newData);
   } catch (error) {
     console.error('Error fetching data:', error);
     throw new Error('Error fetching data');
@@ -27,11 +32,14 @@ const fetchData = async () => {
 const saveDataToMongoDB = asyncHandler(async (req, res) => {
   try {
     await fetchData();
+    console.log("Data Saved");
     res.status(200).json({ message: 'Data Saved to MongoDB' });
   } catch (error) {
-    res.status(400).json({ message: 'Error Saving the Data' });
+    console.error('Error saving data to MongoDB:', error);
+    res.status(500).json({ message: 'Error saving data to MongoDB' });
   }
 });
+
 
 module.exports = {
   saveDataToMongoDB,
--- backend/models/aquaModel.js
+++ backend/models/aquaModel.js
@@ -11,7 +11,7 @@ const aquaSchema = mongoose.Schema(
       required: true,
     },
     time: {
-      type: Number,
+      type: String,
       required: true,
     },
   },
@@ -20,4 +20,4 @@ const aquaSchema = mongoose.Schema(
   }
 );
 
-module.exports = mongoose.model("Aqua", aquaSchema);
+module.exports = mongoose.model("Data", aquaSchema);
