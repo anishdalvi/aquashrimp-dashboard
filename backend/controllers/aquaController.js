65557aebf64cc18630ad2db47e8869a18758f22d
anishDalvi <anishdalvi2001@gmail.com>
2024-03-04 03:34:02 +0530

    edit 6

--- backend/controllers/aquaController.js
+++ backend/controllers/aquaController.js
@@ -73,7 +73,7 @@ const saveDataToMongoDB = asyncHandler(async (req, res) => {
   try {
     const data = await fetchData();
     //console.log("Data Saved");
-    res.status(200).json({ message: "Data Saved to MongoDB", data });
+    res.status(200).json(data);
   } catch (error) {
     console.error("Error saving data to MongoDB:", error);
     res.status(500).json({ message: "Error saving data to MongoDB" });
